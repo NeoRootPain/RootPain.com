7b288b307d06d0cabd66692b8dd4bfb5fd6844c2
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-03-13 18:24:22 -0300

    Add files via upload

---
+++ fanart/pages/BeatumPopcorn/thefirstencounterlogo.js
@@ -0,0 +1,7 @@
+let LogoContent = `
+<div style="display:flex;background-image:url('../../../images/logos/bigwhiskeylogoyellow.gif');margin-right:auto;px;width:487px;height:150px;"></div>
+<div style="display:flex;position:absolute;width:306px;height:78px;">
+<a href="../../../index.html"><img src="../../../images/logos/logogreen.png" class="logo"></a></div>
+
+`;
+document.querySelector('#logocontainer').insertAdjacentHTML('beforeend', LogoContent);
