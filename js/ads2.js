b4b62aeec996f5f2e5bf992394a5f164ef2f0cb9
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-03-23 14:51:10 -0300

    Delete ads2.js

--- js/ads2.js
+++
@@ -1,7 +0,0 @@
-let AdContent = `
-<a href="https://twitter.com/DreamcastCollab">
-<img src="../../../images/dreamcastcollabad.png" class="button" style="outline: solid black 1px;"></a>
-<a href="../../../donate.html">
-<img src="../../../images/emptyadspace.gif" class="button" style="outline: solid black 1px;"></a>
-`;
-document.querySelector('#adscontainer').insertAdjacentHTML('beforeend', AdContent);
