06397d7eebfef7357e5922e26408a3f24fff0bea
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-04-21 02:51:27 -0300

    Add files via upload

---
+++ downloads/pages/doomwads/logoevil.js
@@ -0,0 +1,6 @@
+let LogoContent = `
+<div style="display:flex;position:absolute;width:306px;height:78px;">
+<a href="../../../index.html"><img src="../../../images/logos/logo.png" class="logo"></a></div>
+
+`;
+document.querySelector('#logocontainer').insertAdjacentHTML('beforeend', LogoContent);
