ec481718c67f6783a3227e5b44b608c0b65b3f9a
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-04-23 14:24:58 -0300

    Add files via upload

---
+++ fanart/pages/BeatumPopcorn/randomstuff01logo.js
@@ -0,0 +1,7 @@
+let LogoContent = `
+<div style="display:flex;background-image:url('../../../images/logos/bigwhiskeylogoyellow2.gif');margin-right:600px;px;width:133px;height:150px;"></div>
+<div style="display:flex;position:absolute;width:306px;height:78px;">
+<a href="../../../index.html"><img src="../../../images/logos/logoyellow.png" class="logo"></a></div>
+
+`;
+document.querySelector('#logocontainer').insertAdjacentHTML('beforeend', LogoContent);
