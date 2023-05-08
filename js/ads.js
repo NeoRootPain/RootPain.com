7829b27ccf6f2fdf788923afec52c8ed3c5045dd
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-05-08 15:14:48 -0300

    Add files via upload

--- js/ads.js
+++ js/ads.js
@@ -1,6 +1,4 @@
 let AdContent = `
-<a href="https://twitter.com/DreamcastCollab">
-<img src="images/dreamcastcollabad.png" class="button" style="outline: solid black 1px;"></a>
 <a href="donate.html">
 <img src="images/emptyadspace.gif" class="button" style="outline: solid black 1px;"></a>
 `;
