4ba3dc4c28b6c9506dc82e5203a4de3c6a2115f6
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-08-11 12:30:22 -0300

    Add files via upload

--- js/logogreen.js
+++ js/logogreen.js
@@ -1,5 +1,5 @@
 let LogoContent = `
-<div style="display:flex;background-image:url('https://github.com/JuneSSai/RootPain.com/blob/main/images/logos/bigwhiskeylogogreen.gif?raw=true');margin-left:650px;width:298px;height:150px;"></div>
+<div style="display:flex;background-image:url('https://github.com/JuneSSai/RootPain.com/blob/main/images/logos/bigwhiskeylogogreen.gif?raw=true');margin-left:654px;width:298px;height:150px;"></div>
 <div style="display:flex;position:absolute;width:306px;height:78px;">
 <a href="https://rootpain.com/index.html"><img src="https://github.com/JuneSSai/RootPain.com/blob/main/images/logos/logogreen.png?raw=true" class="logo"></a></div>
 
