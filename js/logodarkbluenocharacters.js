36ddbb35468055cddc5504e5a759c0813a455a33
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-08-24 22:55:13 -0300

    Add files via upload

--- js/logodarkbluenocharacters.js
+++ js/logodarkbluenocharacters.js
@@ -1,5 +1,4 @@
 let LogoContent = `
-<div style="display:flex;background-image:url('https://github.com/JuneSSai/RootPain.com/blob/main/images/logos/bigwhiskeylogodarkblue.gif?raw=true');margin-left:655px;width:281px;height:150px;"></div>
 <div style="display:flex;position:absolute;width:auto;height:auto;">
 <a href="index.html"><img src="https://github.com/JuneSSai/RootPain.com/blob/main/images/logos/logodarkblue.png?raw=true" class="logo"></a></div>
 
