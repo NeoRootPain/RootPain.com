b1c7dfb6daf6a72319fd1bf3c4cd501a372f25c0
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-08-30 14:32:56 -0300

    Add files via upload

---
+++ js/logogreennocharacters.js
@@ -0,0 +1,6 @@
+let LogoContent = `
+<div style="display:flex;position:absolute;width:auto;height:auto;">
+<a href="https://rootpain.com/index.html"><img src="https://github.com/JuneSSai/RootPain.com/blob/main/images/logos/logogreen.png?raw=true" class="logo"></a></div>
+
+`;
+document.querySelector('#logocontainer').insertAdjacentHTML('beforeend', LogoContent);
