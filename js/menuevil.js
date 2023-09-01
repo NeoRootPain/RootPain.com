642956205f9f743f5b65850402f61f940ea0aa48
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2023-09-01 16:49:52 -0300

    Add files via upload

--- js/menuevil.js
+++ js/menuevil.js
@@ -2,51 +2,51 @@ let LeftMenuContent = `
 		<div class="navbarcontainer">
 		<div class="navigationbar" style="background-image:url('../../../images/sidemenus/navigationbarblack.png');"></div>
 		<a href="../../../index.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../blogmain.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../downloadsmain.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../artmain.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../fanartmain.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../links.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../donate.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../guestbook.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../changelog.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="https://junessai.github.io/Old-JuneSSai.net-Archive/">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../about.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<div class="handlebarside" style="background-image:url('../../../images/sidemenus/handlebarsideblack.png');"></div>
 		</div>
 
 		<div class="affbarcontainer">
 			<div class="affiliatesbar" style="background-image:url('../../../images/sidemenus/affiliatesbarblack.png');"></div>
 		<a href="https://gameslime.com/" target="_blank">
-			<img src="../../../images/buttons/evilbutton.png" class="button"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="https://sagerankov.com/" target="_blank">
-			<img src="../../../images/buttons/evilbutton.png" class="button"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="https://qmaury.com/" target="_blank">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="https://www.storkinspork.com/" target="_blank">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<a href="../../../links.html">
-			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 		<div class="handlebarside" style="background-image:url('../../../images/sidemenus/handlebarsideblack.png');"></div>
 		</div>
 
 		<div class="socialsbarcontainer">
 			<div class="socialsbar" style="background-image:url('../../../images/sidemenus/socialsbarblack.png');"></div>
 			<a href="https://junessai.newgrounds.com/">
-				<img src="../../../images/buttons/evilbutton.png" class="button"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 			<a href="https://discord.gg/tYxjnQBpxm" target="_blank">
-				<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:-1px;"></a>
+			<img src="../../../images/buttons/evilbutton.png" class="button" style="margin-top:1px;"></a>
 			<div class="handlebarside" style="background-image:url('../../../images/sidemenus/handlebarsideblack.png');"></div>
 		</div>
 `;
